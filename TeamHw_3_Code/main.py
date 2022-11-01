68f2795206204802dc972235f448409c0833a20e
y8jinn <jyj010530@dgu.ac.kr>
2022-11-01 13:08:55 +0900

    MODIFY languages function

---
+++ TeamHw_3_Code/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')  # default URL
+def student():
+   return render_template('input_info.html')
+
+@app.route('/result', methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = dict()
+      result['Name'] = request.form.get('Name')
+
+   if request.method == 'POST':
+      result = dict()
+      result['Student Number'] = request.form.get('Student Number')
+
+      return render_template("result.html",result = result)
+
+if __name__ == '__main__':
+   app.run()
